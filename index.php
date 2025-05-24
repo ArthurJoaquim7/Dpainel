110a9ec3465941811f7a2b05405538f1a2c27bfd
Arthur <arthurkhlf00@gmail.com>
2025-05-24 15:18:49 -0300

    Conectando com DB

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+<?php 
+require 'conexao.php';
+?>
+
 <!doctype html>
-<html lang="pt-BR">
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>D-painel</title>
+    <title>Dpainel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 
